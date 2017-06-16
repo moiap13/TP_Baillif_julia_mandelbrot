552af1dc2dc53c7655d4b7c9b4bd0e38d7b0ca66
Antonio Pisanello <moiap13@gmail.com>
2017-06-16 17:41:59 +0200

    Add files via upload

--- mandelbrot.m
+++ mandelbrot.m
@@ -3,16 +3,16 @@ clear all
 a = -0.7485005;   # Declaration of the variable a
 b = 0.099948;     # Declaration of the variable b
 
-array_size = 500; # Size of the grid
-iteration = 300; # Number of iterationss
+array_size = 1000; # Size of the grid
+iteration = 500; # Number of iterationss
 
 q = 2;
 
 % Ensemble complet
-x_1 = -0.6;      # Limit pour les fonctions linspace
-x_2 = -0.7;      # Limit pour les fonctions linspace
-y_1 = -0.2;      # Limit pour les fonctions linspace
-y_2 = 0;      # Limit pour les fonctions linspace
+x_1 = -2;      # Limit pour les fonctions linspace
+x_2 = 2;      # Limit pour les fonctions linspace
+y_1 = -1;      # Limit pour les fonctions linspace
+y_2 = 1;      # Limit pour les fonctions linspace
 
 x = linspace(x_1,x_2,array_size); 
 y = linspace(y_1,y_2,array_size);
@@ -28,5 +28,8 @@ for t = 1:iteration
 	z = ((z.^2)+c);
 endfor
 
-colormap(jet)
+colormap([jet() ; flipud(ocean())])
 imagesc(x,y,abs(m))
+
+title('X[-2;2] ; Y[-1;1]');
+ print("mandelbrot_1.png");
